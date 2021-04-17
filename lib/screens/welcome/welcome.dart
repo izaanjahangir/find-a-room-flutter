7938cbe398089bd6084a3d2aa0885b2f8462e7e3
izaanjahangir <izaanjahangir2@gmail.com>
2021-04-17 16:34:14 +0500

    code cleanup

---
+++ lib/screens/welcome/welcome.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class Welcome extends StatelessWidget {
+  static const String screenName = "/welcome";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Welcome"),
+    );
+  }
+}
