cccbc10ed245b0cd64807baaa363ca99b5597f3d
izaanjahangir <izaanjahangir2@gmail.com>
2021-04-19 22:44:14 +0500

    working on home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,22 @@
 import "package:flutter/material.dart";
+import 'package:izaan_want_a_room/screens/home/home_item.dart';
 import 'package:izaan_want_a_room/utils/helpers.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   static const String screenName = "/home";
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = new TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +30,64 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SingleChildScrollView(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(child: Text("Home")),
-                    ],
-                  ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    SingleChildScrollView(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                                child: Column(
+                              children: [
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                                HomeItem(),
+                              ],
+                            )),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text("Profile"),
+                  ],
+                  controller: _tabController,
                 ),
-              )
+              ),
+              TabBar(
+                unselectedLabelColor: Colors.black,
+                labelColor: Colors.red,
+                tabs: [
+                  Tab(
+                    text: 'Home',
+                  ),
+                  Tab(
+                    text: 'Profile',
+                  ),
+                ],
+                controller: _tabController,
+                indicatorSize: TabBarIndicatorSize.tab,
+              ),
             ],
           ),
         )),
---
+++ lib/screens/home/home_item.dart
@@ -0,0 +1,69 @@
+import "package:flutter/material.dart";
+import 'package:izaan_want_a_room/config/theme_colors.dart';
+
+class HomeItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: PhysicalModel(
+        color: ThemeColors.black,
+        elevation: 4,
+        child: Material(
+          color: ThemeColors.white,
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              height: size.width * 0.2,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/room.jpeg"),
+                    height: size.width * 0.2,
+                    width: size.width * 0.35,
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Malir, karachi",
+                              maxLines: 2,
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            Text(
+                              "Distance: 30KM",
+                              style: TextStyle(
+                                  fontSize: 12, color: ThemeColors.darkBlue),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          "\$200 / month",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: ThemeColors.darkBlue,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
